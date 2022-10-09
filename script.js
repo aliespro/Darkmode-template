7e7ed7a7174b1c88beee127c086fa09201924cbf
amirali <amirali.programer2017@gmail.com>
2022-10-09 14:47:19 +0330

    ok moon and sun

--- script.js
+++ script.js
@@ -10,12 +10,11 @@ function check() {
         document.getElementById("button-choose").style.boxShadow = "2px 2px 10px #e2e2e2 ";
         document.getElementById("light").style.color = "#14141a ";
         document.getElementById("dark").style.color = "#848487 ";
-
         document.getElementById("p-h").style.color = "#14141a ";
         document.getElementById("p-p").style.color = "#14141a ";
         document.getElementById("circle").style.background = "linear-gradient(243deg, #fd7616, #fd4949) ";
-        document.getElementById("sun").style.left = "16px";
-        document.getElementById("sun").style.top = "3px";
+        document.getElementById("sun").style.left = "0px";
+
 
         document.getElementById("phone").style.boxShadow = "6px 6px 12px #7676764f, -6px -6px 12px #7676764f";
 
@@ -23,6 +22,8 @@ function check() {
         document.getElementById("sun").style.background = " linear-gradient(243deg, #fd7616, #fd4949)";
         document.getElementById("header").style.color = "#222 ";
         document.getElementById("header").style.opacity = "30%";
+
+
     }
     if (checkbox.checked == true) {
         document.body.style.background = "#201c27";
@@ -42,8 +43,11 @@ function check() {
         document.getElementById("phone").style.boxShadow = "-2px 6px 12px #2b292975, -2px -6px 12px #2b292975";
 
         document.getElementById("moon").style.background = "linear-gradient(45deg, rgb(129, 110, 253), rgb(133, 178, 253))";
-        document.getElementById("sun").style.background = "#201c27";
         document.getElementById("circle").style.background = "linear-gradient(45deg, #816efd, #85b2fd) ";
+
+
+
+        animte_moon();
     }
 
     function anmite() {
@@ -58,20 +62,43 @@ function check() {
                 document.getElementById("circle").style.opacity = pos + "%";
             }
         }
-        let size = 50;
-        const time = setInterval(fram_moon_sun, 7);
+    }
 
-        function fram_moon_sun() {
-            if (size == 78) {
-                clearInterval(id);
-            } else {
-                size++;
-                document.getElementById("sun").style.width = size + "px";
-                document.getElementById("sun").style.height = size + "px";
+    function animte_moon() {
+
+        const time = setInterval(fram_moon, 6);
+        let size_moon = 20;
+        let size_sun = 70;
+
+        function fram_moon() {
+
+
+            if (checkbox.checked == true) {
+                if (size_moon == 78) {
+                    clearInterval(size_moon);
+                } else {
+                    size_moon++;
+                    document.getElementById("sun").style.background = "#201c27";
+                    document.getElementById("sun").style.width = size_moon + "px";
+                    document.getElementById("sun").style.height = size_moon + "px";
+
+                }
+
+            } else if (checkbox.checked == false) {
+                if (size_sun == 0) {
+                    clearInterval(size_sun);
+                } else {
+
+                    size_sun--;
+                    document.getElementById("sun").style.background = "#fbfbfb";
+                    document.getElementById("sun").style.width = size_sun + "px";
+                    document.getElementById("sun").style.height = size_sun + "px";
+
+
+                }
 
             }
         }
-
     }
 
     anmite();
